15a72523d727e46c1945dcc2960399a0c4c1b782
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-11-06 12:28:41 -0800

    adding a stop works w/ address populating

--- e2e/tours.js
+++ e2e/tours.js
@@ -1,7 +1,7 @@
 /* eslint-disable-next-line*/
 const router = require('express').Router();
 const Tour = require('../lib/models/Tour');
-
+const getLocation = require('../lib/middleware/zip-code-api');
 
 router
   .post('/', (req, res, next) => {
@@ -12,6 +12,11 @@ router
   .get('/', (req, res, next) => {
     Tour.find()
       .then(tour => res.json(tour))
+      .catch(next);
+  })
+  .post('/:id/stops', getLocation(), ({ params, body }, res, next) => {
+    Tour.addStop(params.id, body)
+      .then(addedStop => res.json(addedStop))
       .catch(next);
   });
  
